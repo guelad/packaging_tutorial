b3bb9a65f1b1d6ba1178bc0c62b6c659c1dd5d62
guelad <niassegueladio@gmail.com>
2021-03-17 00:46:41 +0100

    commit

--- packaging_tutorial/biketrauma/preprocess/get_accident.py
+++ packaging_tutorial/biketrauma/preprocess/get_accident.py
@@ -1,2 +1,13 @@
-url = https://github.com/bcharlier/HMMA238/blob/master/Courses/Python-modules/modules_files/get_accident.py
-path_target = get_accident.py
+import  pandas as pd
+import numpy as np
+
+
+def get_accident(df_bikes, log_scale=True):
+  gd = df_bikes.groupby(['departement']).size()
+  if log_scale:
+    gd = np.log(gd)
+  return gd
+
+# on a le lien 
+#url = https://github.com/bcharlier/HMMA238/blob/master/Courses/Python-modules/modules_files/get_accident.py
+#path_target = get_accident.py 
