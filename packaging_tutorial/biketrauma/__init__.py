cac41568a9b0e304374734e55af96724e7984c88
guelad <niassegueladio@gmail.com>
2021-03-17 00:39:19 +0100

    commit modif

---
+++ packaging_tutorial/biketrauma/__init__.py
@@ -0,0 +1,3 @@
+from .io.Load_db import Load_db
+from .vis.plot_location import plot_location
+from .preprocess.get_accident import get_accident
---
+++ packaging_tutorial/biketrauma/preprocess/get_accident.py
@@ -0,0 +1,2 @@
+url = https://github.com/bcharlier/HMMA238/blob/master/Courses/Python-modules/modules_files/get_accident.py
+path_target = get_accident.py
